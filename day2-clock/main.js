2b4e510f1f8b8464918dbc8a9317f6c0774e50e3
peayah <82358003+peayah@users.noreply.github.com>
2022-06-18 16:46:55 -0700

    console log

--- day2-clock/main.js
+++ day2-clock/main.js
@@ -2,8 +2,11 @@
 function setDate() {
   const now = new Date ();
   const second = now.getSeconds()
+  console.log(second)
   const minute = now.getMinutes()
+  console.log(minute)
   const hour = now.getHours()
+  console.log(hour)
 }
 
 setInterval(setDate, 1000);
