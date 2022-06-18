f74ed88ad1a161594db02d1aef7349648b90832c
peayah <82358003+peayah@users.noreply.github.com>
2022-06-18 16:29:34 -0700

    add seconds, mins, hours

--- day2-clock/main.js
+++ day2-clock/main.js
@@ -1,1 +1,9 @@
 
+function setDate() {
+  const now = new Date ();
+  const second = now.getSeconds()
+  const minute = now.getMinutes()
+  const hour = now.getHours()
+}
+
+setInterval(setDate, 1000);
