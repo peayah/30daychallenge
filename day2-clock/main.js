9da9527e06cc99e377a521cc66e071424190a843
peayah <82358003+peayah@users.noreply.github.com>
2022-06-18 19:02:29 -0700

    add end "

--- day2-clock/main.js
+++ day2-clock/main.js
@@ -1,6 +1,6 @@
 const secondHand = document.querySelector(".sec-hand");
 const minuteHand = document.querySelector(".min-hand");
-const hourHand = document.querySelector(".hour-hand);
+const hourHand = document.querySelector(".hour-hand");
                                         
 function setDate() {
   
