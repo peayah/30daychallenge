b7649f769acdf64c02aa6c57d9327a3db86d98c7
peayah <82358003+peayah@users.noreply.github.com>
2022-06-18 18:28:50 -0700

    add min and hour movement

--- day2-clock/main.js
+++ day2-clock/main.js
@@ -1,15 +1,25 @@
 const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const hourHand = document.querySelector(".hour-hand
 function setDate() {
   
   
   const now = new Date ();
+  // seconds movment
   const second = now.getSeconds()
   const secondsDegrees = ((second / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  
+  // minutes movement
   const minute = now.getMinutes()
   const minutesDegrees = ((minutes / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  
+  //hours movement
   const hour = now.getHours()
   const hourDegrees = ((hours / 12) * 360) +90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
+
