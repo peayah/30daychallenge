66a67fe13c79746db48bd4cb863a21488cd5739c
peayah <82358003+peayah@users.noreply.github.com>
2022-06-18 17:47:58 -0700

    add seconds hands movement

--- day2-clock/main.js
+++ day2-clock/main.js
@@ -1,8 +1,11 @@
-
+const secondHand = document.querySelector(".second-hand");
 function setDate() {
+  
+  
   const now = new Date ();
   const second = now.getSeconds()
-  console.log(second)
+  const secondsDegrees = ((seconds / 60) *360) +90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   const minute = now.getMinutes()
   console.log(minute)
   const hour = now.getHours()
