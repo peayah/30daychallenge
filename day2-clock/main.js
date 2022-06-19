67da18614c5d152c840bba4b6f0f7a87954aa0f7
peayah <82358003+peayah@users.noreply.github.com>
2022-06-18 19:09:49 -0700

    correct spelling

--- day2-clock/main.js
+++ day2-clock/main.js
@@ -1,5 +1,5 @@
-const secondHand = document.querySelector(".sec-hand");
-const minuteHand = document.querySelector(".min-hand");
+const secHand = document.querySelector(".sec-hand");
+const minHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
                                         
 function setDate() {
@@ -9,17 +9,17 @@ function setDate() {
   
   // seconds movment
   const second = now.getSeconds()
-  const secondsDegrees = ((second / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const secDegrees = ((second / 60) * 360) + 90;
+  secHand.style.transform = `rotate(${secDegrees}deg)`;
   
   // minutes movement
   const minute = now.getMinutes()
-  const minutesDegrees = ((minutes / 60) * 360) + 90;
-  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  const minDegrees = ((minute / 60) * 360) + 90;
+  minHand.style.transform = `rotate(${minDegrees}deg)`;
   
   //hours movement
   const hour = now.getHours()
-  const hourDegrees = ((hours / 12) * 360) +90;
+  const hourDegrees = ((hour / 12) * 360) +90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
