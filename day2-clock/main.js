5ad2e95dd791b3ea2f1ab603d333c9c28fd7d99c
peayah <82358003+peayah@users.noreply.github.com>
2022-06-18 18:39:34 -0700

    set correct class names

--- day2-clock/main.js
+++ day2-clock/main.js
@@ -1,5 +1,5 @@
-const secondHand = document.querySelector(".second-hand");
-const minuteHand = document.querySelector(".minute-hand");
+const secondHand = document.querySelector(".sec-hand");
+const minuteHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand);
                                         
 function setDate() {
