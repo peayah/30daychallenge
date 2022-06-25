c250723760f02e4e340429869f7a0c68bb9f1f3d
peayah <82358003+peayah@users.noreply.github.com>
2022-06-24 23:15:12 -0700

    add brush settings and image to on main page

--- day8-canvas/main.js
+++ day8-canvas/main.js
@@ -6,17 +6,49 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
+ctx.lineWidth = 20;
+
+//blends colors
+// ctx.globalCompositeOperation = "multiply"
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
-function draw(event) {
+function draw(e) {
     if(!isDrawing) return // funtion will not run when no mouse down
-    console.log(event)
+    console.log(e)
+    //https://mothereffinghsl.com
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+
+    ctx.beginPath()
+    ctx.moveTo(lastX, lastY)
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    lastX = e.offsetX;
+    lastY = e.offsetY
+    hue++
+
+    if (hue >= 360) {
+        hue = 0;
+    }
+    if (ctx.lineWidth >= 50 || ctx.lineWidth <=1) {
+        direction = !direction
+    }
+
+    if(direction) {
+        ctx.lineWidth++
+    } else {
+        ctx.lineWidth--
+    }
 }
 
 canvas.addEventListener('mousemove', draw)
-canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY]
+});
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
