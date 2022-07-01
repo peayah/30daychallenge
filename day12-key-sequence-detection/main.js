881d27d55630fad72ad6e8565f8652b5622bc7ef
peayah <82358003+peayah@users.noreply.github.com>
2022-07-01 15:55:58 -0700

    add more animals

--- day12-key-sequence-detection/main.js
+++ day12-key-sequence-detection/main.js
@@ -1,12 +1,19 @@
 const pressed = [];
-const secretCode = 'elephant';
+const secretCodeOne = 'elephant';
+const secretCodeTwo = "whale"
+const secretCodeThree = "tiger"
+const secretCodeFour = "lion"
+const secretCodeFive = "dog"
 
 window.addEventListener('keyup', (e) => {
   console.log(e.key);
   pressed.push(e.key);
-  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
-  if (pressed.join('').includes(secretCode)) {
-    console.log('DING DING!');
+  pressed.splice(secretCodeOne.length - 1, pressed.length - secretCodeOne.length);
+  if ((pressed.join('').includes(secretCodeOne)) || (pressed.join('').includes(secretCodeTwo)) || (pressed.join('').includes(secretCodeThree)) || (pressed.join('').includes(secretCodeThree)) || (pressed.join('').includes(secretCodeFour)) || (pressed.join('').includes(secretCodeFive))){
+    console.log('You guessed it!');
+    const youDidIt = "You did it"
+    document.querySelector('h1').innerText.youDidIt
+
     cornify_add();
   }
   console.log(pressed);
